4bd1e99531049c5504876be6034dafb4a0bac41b
Anik Sen <cse2023anik@gmail.com>
2024-03-25 03:50:08 +0600

    reverse_string2.cpp

---
+++ reverse_string2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  string str;
+  getline(cin, str);
+  string str_rev;
+  for(int i=str.size()-1; i>=0; i--){
+    str_rev.push_back(str[i]);
+  }
+  cout<<str_rev;
+  return 0;
+}
+//It is less time complexity
